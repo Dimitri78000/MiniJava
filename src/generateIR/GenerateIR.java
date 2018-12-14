ec444fe8b73499277a19fcd6e68be21af6751838
Dimitri Leurs <dimitrileurs@MacBook-Air-de-Dimitri.local>
2018-12-14 14:13:21 +0100

    Debut Compil 4

--- src/generateIR/GenerateIR.java
+++ src/generateIR/GenerateIR.java
@@ -3,46 +3,48 @@ package generateIR;
 import AST.*;
 import IR.*;
 import symboltable.*;
-import semantic.SemanticAttrbut;
-import semantic.SemanticTree;
 
 public class GenerateIR extends ASTVisitorDefault { 
-    public Intermediate intermed;	
-    private SemanticAttrbut<Variable> nodeVar;  // nom Variables dans IR	
+    public Intermediate intermed;   
+    private semantic.SemanticTree semanticTree; // only for lookup in symbolTable
+    private semantic.SemanticAttrbut<Variable> nodeVar;  // nom Variables dans IR       
     
-    public GenerateIR(SemanticTree semanticTree){
-	this.intermed=new Intermediate(semanticTree.rootScope);
-	this.nodeVar=new  SemanticAttrbut<Variable>();
-	semanticTree.axiome.accept(this); //=> visite((Raxiome)axiome) 
-	if ( main.DEBUG.INTERMED ) {
-	    System.out.println("=== INTERMEDIATE REPRESENTATION ===");
-	    intermed.program.print(); 
-	}
+    public GenerateIR(semantic.SemanticTree semanticTree){
+        this.intermed=new Intermediate(semanticTree);
+        this.semanticTree = semanticTree;
+        this.nodeVar=new semantic.SemanticAttrbut<Variable>();
+        
+        semanticTree.axiome.accept(this); //=> visite((Raxiome)axiome) 
+        
+        if ( main.DEBUG.INTERMED ) {
+            System.out.println("=== INTERMEDIATE REPRESENTATION ===");
+            intermed.program.print(); 
+        }
     }
     
-    // helpers
-    private Variable newLabel(String name) {return new Variable(name,"label");}
-    private Variable newConst(String name) { return new Variable(name, "constant"); }
+// Helpers
+    // Ajouter une instruction au programmeIR
+    private void add(IRQuadruple irq)  { intermed.program.add(irq); }
+    // Attribut synthetisee nodeVar = Variable IR Temp pour resultat des expression
     private Variable getVar(ASTNode n) { return nodeVar.getAttr(n); }
-    private Variable setVar(ASTNode n, Variable var) { return nodeVar.setAttr(n, var);}	
-    private void add(IRQuadruple irq) { intermed.program.add(irq); }
-    
-    /////////////////// Visit ////////////////////
+    private Variable setVar(ASTNode n, Variable var) { return nodeVar.setAttr(n, var);}
+    // Variables IR : label tempo, label nom, Constante, Temp var
+    private Variable newLabel()            { return intermed.newLabel(); }
+    private Variable newLabel(String name) { return intermed.newLabel(name); }
+    private Variable newConst(String name) { return intermed.newConst(name); }
+    private Variable newTemp(ASTNode n)    { return intermed.newTemp(n); }   
+    // Variable de l'AST depuis la table de symbole
+    private Variable lookupVar(ASTNode n, String name){
+        return semanticTree.getScope(n).lookupVariable(name); 
+    }
+ 
+/////////////////// Visit ////////////////////
+    /** RklassMain : RidKlass id; RidVar arg; Pinst s; */
     public void visit(RklassMain n) {
-        add(new QLabel(newLabel("main")));		
-    	defaultVisit(n);
-	add(new QCall(	newLabel("_system_exit"),
-			newConst("0"),
-			null));
-    }
-    public void visit(REint n) {
-	setVar(n, newConst(n.i.toString()));
-    }
-    public void visit(RIprint n) {
-    	defaultVisit(n);	   	
-	add(new QParam(getVar(n.e)));
-	add(new QCall(newLabel ("_system_out_println"),
-		      newConst("1"),
-		      null));
+       add(new QLabel(newLabel("main")));               
+        defaultVisit(n);
+        add(new QCall(  newLabel("_system_exit"),
+                        newConst("0"),
+                        null));
     }
 }
--- src/generateIR/Intermediate.java
+++ src/generateIR/Intermediate.java
@@ -1,15 +1,50 @@
 package generateIR;
-import symboltable.Variable; // reutilise pour les variables IR
+import java.util.ArrayList;
+import symboltable.*;
 
 public class Intermediate {
-    public IR.IRProg program;            // programme intermédiaire
-    public symboltable.Scope rootScope;  // table de symbole
-    public java.util.ArrayList<Variable> varList; // variables IR
-    
-    public Intermediate(symboltable.Scope rootScope) {
-        this.rootScope = rootScope;
-        this.program=new IR.IRProg();
-        this.varList= new java.util.ArrayList<Variable>();
-    }
+        public IR.IRProg program;      //
+        public Scope rootScope;     // Table des symboles AST
+        public ArrayList<Variable> constList; // liste des constantes
+        public ArrayList<Variable> labelList; // liste des Label
+        public ArrayList<Variable> tempList;  // liste des variablesIR
 
+        private semantic.SemanticTree semanticTree; // only for lookup symbol
+    private int labelNum;  // numerotation des labels temporaires
+    private int varNum;   // numerotation des variables temporaires
+
+        public Intermediate(semantic.SemanticTree semanticTree) {
+                this.semanticTree = semanticTree;
+                this.labelNum=0;
+                this.varNum=0;
+                
+                this.program  = new IR.IRProg();
+                this.rootScope= semanticTree.rootScope;
+                this.constList= new ArrayList<Variable>();
+                this.labelList= new ArrayList<Variable>();
+                this.tempList = new ArrayList<Variable>();
+        }
+        
+        /* Variables IR = Constante/immediate , Labels , Temporaire */
+     public Variable newLabel() { //label tempo
+                return newLabel("L"+labelNum++);
+        }
+    public Variable newLabel(String name) {  //label method
+        Variable v = new Variable(name, "label");
+        labelList.add(v);
+        return v;
+    }
+    public Variable newConst(String name) { 
+        Variable v=new Variable(name, "constant");
+        constList.add(v);
+        return v;
+    }
+    public Variable newTemp(AST.ASTNode n) { 
+        String methname= semanticTree.getScope(n).currentMethod().name;
+        Variable v=new Variable("t"+varNum++, methname);
+        tempList.add(v);
+        return v;
+    }
+      
 }
+
